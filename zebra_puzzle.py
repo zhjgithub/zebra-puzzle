62e1750fbe4d569094dc2c1a56e1cf7899327096
organism <neuqzhj@live.com>
2018-01-01 15:04:51 +0800

    feature: Implement zebra puzzle function

--- zebra_puzzle.py
+++ zebra_puzzle.py
@@ -5,9 +5,6 @@ Zebra Puzzle
 
 import itertools
 
-HOUSES = [1, 2, 3, 4, 5]
-orderings = list(itertools.permutations(HOUSES))
-
 
 def is_right(house1, house2):
     '''
@@ -23,11 +20,31 @@ def next_to(house1, house2):
     return abs(house1 - house2) == 1
 
 
-for (red, green, ivory, yellow, blue) in orderings:
-    for (Englishman, Spaniard, Ukrainian, Japanese, Norwegian) in orderings:
-        for (dog, snails, fox, horse, ZEBRA) in orderings:
-            for (coffee, tea, milk, oj, WATER) in orderings:
+def zebra_puzzle():
+    '''
+    Return a tuple (WATER, ZEBRA) indicating their house numbers.
+    '''
+    houses = first, _, middle, _, _ = [1, 2, 3, 4, 5]
+    orderings = list(itertools.permutations(houses))  # 1
+    return next((WATER, ZEBRA)
+                for (red, green, ivory, yellow, blue) in orderings
+                for (Englishman, Spaniard, Ukrainian, Japanese,
+                     Norwegian) in orderings
+                for (dog, snails, fox, horse, ZEBRA) in orderings
+                for (coffee, tea, milk, oj, WATER) in orderings
                 for (OldGold, Kools, Chesterfields, LuckyStrike,
-                     Parliaments) in orderings:
-                    if Englishman is red:
-                        pass
+                     Parliaments) in orderings
+                if Englishman is red  # 2
+                if Spaniard is dog  # 3
+                if coffee is green  # 4
+                if Ukrainian is tea  # 5
+                if is_right(green, ivory)  # 6
+                if OldGold is snails  # 7
+                if Kools is yellow  # 8
+                if milk is middle  # 9
+                if Norwegian is first  # 10
+                if next_to(Chesterfields, fox)  # 11
+                if Kools is horse  # 12
+                if LuckyStrike is oj  # 13
+                if Japanese is Parliaments  # 14
+                if next_to(Norwegian, blue))  #15
