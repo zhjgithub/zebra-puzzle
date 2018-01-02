a003da283dda1209e3acf1d7699d777d1538503d
organism <neuqzhj@live.com>
2018-01-02 12:00:06 +0800

    feature: Add a faster implement of cryptarithmetic

--- cryptarithmetic.py
+++ cryptarithmetic.py
@@ -28,7 +28,49 @@ def fill_in(formula):
         yield formula.translate(table)
 
 
+def faster_solve(formula):
+    '''
+    Given a formula like 'ODD + ODD == EVEN', fill in digits to solve it.
+    Input formula is a string; output is a digit-filled-in string or None.
+    This version precompiles the formula; only one eval per formula.
+    '''
+    f, letters = complie_formula(formula)
+    for digits in itertools.permutations((1, 2, 3, 4, 5, 6, 7, 8, 9, 0),
+                                         len(letters)):
+        try:
+            if f(*digits) is True:
+                table = str.maketrans(letters, ''.join(map(str, digits)))
+        except ArithmeticError:
+            pass
+
+
+def complie_formula(formula, verbose=False):
+    '''
+    Compile formula into a function. Also return letters found, as a str, in same order as params of function. For example, 'YOU == ME**2' returns (lambda Y, M, E, U, O: (U+10*O+100*Y) == (E+10*M)**2), 'YMEUO'
+    '''
+    letters = ''.join(set(re.findall('[A-Z]', formula)))
+    params = ', '.join(letters)
+    tokens = map(compile_word, re.split('([A-Z]+)', formula))
+    body = ''.join(tokens)
+    f = 'lambda {}: {}'.format(params, body)
+    if verbose:
+        print(f)
+    return eval(f), letters
+
+
+def compile_word(word):
+    """Compile a word of uppercase letters as numeric digits.
+    E.g., compile_word('YOU') => '(1*U+10*O+100*Y)'
+    Non-uppercase words unchanged: compile_word('+') => '+'"""
+    if word.isupper():
+        terms = ('{}*{}'.format(10**i, d) for i, d in enumerate(word[::-1]))
+        return '({})'.format('+'.join(terms))
+    else:
+        return word
+
+
 if __name__ == '__main__':
     print(solve('ODD + ODD == EVEN'))
     import cProfile
     cProfile.run("solve('ODD + ODD == EVEN')")
+    cProfile.run("faster_solve('ODD + ODD == EVEN')")
