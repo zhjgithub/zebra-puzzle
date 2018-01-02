55a6148bcafb24096b00206729d0a7f0ba8dff13
organism <neuqzhj@live.com>
2018-01-02 18:35:19 +0800

    feature: Add another implement of longest sub palindrome find function

--- longest_subpalindrome.py
+++ longest_subpalindrome.py
@@ -29,21 +29,48 @@ def is_palindrome_iteration(s):
     return True
 
 
-def test():
+def longest_subpalindrome_slice2(text):
+    "Return (i, j) such that text[i:j] is the longest palindrome in text."
+    if text == '':
+        return 0, 0
+    if len(text) is 1:
+        return 0, 1
+
+    def length(pair):
+        a, b = pair
+        return b - a
+
+    text = ''.join(map(str.upper, text))
+    candidates = (grow(text, start, end)
+                  for start in range(len(text)) for end in (start, start + 1))
+    return max(candidates, key=length)
+
+
+def grow(text, start, end):
+    '''Start with a 0- or 1- length palindrome; try to grow a bigger one.'''
+    while start > 0 and end < len(text) and text[start - 1] == text[end]:
+        start -= 1
+        end += 1
+    return start, end
+
+
+def test(fun=longest_subpalindrome_slice):
     '''
     Test.
     '''
-    L = longest_subpalindrome_slice
-    assert L('racecar') == (0, 7)
-    assert L('Racecar') == (0, 7)
-    assert L('RacecarX') == (0, 7)
-    assert L('Race carr') == (7, 9)
-    assert L('Race car') == (0, 1)
-    assert L('') == (0, 0)
-    assert L('something rac e car going') == (8, 21)
-    assert L('xxxxx') == (0, 5)
-    assert L('Mad am I ma dam.') == (0, 15)
-    return 'tests pass'
+    assert fun('racecar') == (0, 7)
+    assert fun('Racecar') == (0, 7)
+    assert fun('RacecarX') == (0, 7)
+    assert fun('Race carr') == (7, 9)
+    assert fun('Race car') == (0, 1)
+    assert fun('') == (0, 0)
+    assert fun('something rac e car going') == (8, 21)
+    assert fun('xxxxx') == (0, 5)
+    assert fun('Mad am I ma dam.') == (0, 15)
+    # print('tests pass')
 
 
-print(test())
+if __name__ == '__main__':
+    import cProfile
+    cProfile.run('for _ in range(100):test()')
+    cProfile.run('for _ in range(100):test(fun=longest_subpalindrome_slice2)')
